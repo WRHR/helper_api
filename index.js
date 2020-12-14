c8e3aa519e44b5ba08166b32ca02e4007bdcf99d
WRHR <wrhreeves@gmail.com>
2020-12-14 16:38:05 -0700

    Setup orm and db

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const MikroORM = require("@mikro-orm/core");
 
-
-
-const main = () => {
+const main = async () => {
   const app = express();
 
-  dotenv.config()
+  dotenv.config();
 
+  const orm = await MikroORM.init({
+    migrations: {
+      path: path.join(__dirname, "./migrations"),
+      pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [],
+    user: "w",
+    dbName: "helper-db",
+    type: "postgresql",
+  });
 
-  const PORT = process.env.PORT || 8000
-  app.listen( PORT, ()=>console.log(`Server is running on port: ${PORT}...`));
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`));
 };
 
 main();
