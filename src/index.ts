ac975cb4c247339ca76d07567a27391ce387e7d9
WRHR <wrhreeves@gmail.com>
2020-12-14 17:40:27 -0700

    Create apolloServer, create hello resolver to test graphql server

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,29 @@ import "reflect-metadata";
 import express from "express";
 import { MikroORM } from "@mikro-orm/core";
 import mikroConfig from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { MyContext } from "./types";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
